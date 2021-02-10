41984b91f4c56720108c41ee58bc04101626f8de
Nóra Gabriella Szőke <nora.szoke26@gmail.com>
2021-02-10 15:01:12 +0100

    db migration on npm start

---
+++ database/migrations/20210210142545_create_leisure_center_table.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('leisure_centers', function(table) {
+      table.increments('id').primary();
+      table.string('subpage_name');
+      table.text('description');
+      table.text('address');
+      table.text('link');
+      table.string('activity');
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('leisure_centers');
+};
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//const path = require('path');
+const path = require('path');
 const config = require('./api/config');
 
 module.exports = {
@@ -9,5 +9,12 @@ module.exports = {
   pool: {
     min: 2,
     max: 10
+  },
+  migrations: {
+    directory: path.join(__dirname, 'database/migrations'),
+    tableName: 'knex_migrations'
+  },
+  seeds: {
+    directory: path.join(__dirname, 'database/seeds')
   }
 };
